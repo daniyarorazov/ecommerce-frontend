fbffd602779e141d5d8538084762e87e31d85aa2
Daniyar Orazov <orazov.video@gmail.com>
2023-03-08 17:55:04 +0100

    Created React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { Container } from 'react-bootstrap';
-
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
-import HomeScreen from './screens/HomeScreen.jsx';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
 
   return (
-    <div className="App">
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
